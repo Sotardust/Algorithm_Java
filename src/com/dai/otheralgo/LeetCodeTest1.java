356ac3c98468d005584f509e4699c1a552918998
Sotardust <958471597@qq.com>
2019-10-08 16:46:19 +0800

    添加leetcode算法题

--- src/com/dai/otheralgo/LeetCodeTest1.java
+++ src/com/dai/otheralgo/LeetCodeTest1.java
@@ -17,7 +17,14 @@ public class LeetCodeTest1 {
 //        System.out.println("result1 = " + result1);
 //        System.out.println("result2 = " + result2);
         boolean result = isPalindrome(304);
-        System.out.println("result = " + result);
+        System.out.println("result = " + 10 * 8);
+        System.out.println("result = " + (10 ^ 3));
+        System.out.println("result = " + (10 ^ 2));
+        System.out.println("result = " + (1994 / 1000));
+
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+//        maxArea(height);
+        intToRoman(1994);
     }
 
     /**
@@ -170,25 +177,26 @@ public class LeetCodeTest1 {
 
     /**
      * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
-     *
+     * <p>
      * '.' 匹配任意单个字符
      * '*' 匹配零个或多个前面的那一个元素
      * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
-     *
+     * <p>
      * 说明:
      * <p>
      * s 可能为空，且只包含从 a-z 的小写字母。
      * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
-     *
+     * <p>
      * 输入:
      * s = "aa"
      * p = "a*"
      * 输出: true
      * 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
-     *
+     * <p>
      * 来源：力扣（LeetCode）
      * 链接：https://leetcode-cn.com/problems/regular-expression-matching
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
      * @param s
      * @param p
      * @return
@@ -197,5 +205,109 @@ public class LeetCodeTest1 {
 
 
         return true;
+    }
+
+    /**
+     * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+     * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
+     * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     * <p>
+     * 说明：你不能倾斜容器，且 n 的值至少为 2。
+     * <p>
+     * <p>
+     * <p>
+     * 图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入: [1,8,6,2,5,4,8,3,7]
+     * 输出: 49
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/container-with-most-water
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int value = (right - left) * (height[left] > height[right] ? height[right--] : height[left++]);
+            if (value > maxArea) {
+                maxArea = value;
+            }
+            System.out.println("value = " + value + "maxArea = " + maxArea);
+        }
+        return maxArea;
+    }
+
+    /**
+     * 罗马数字包含以下七种字符： I， V， X， L，C，D 和 M。
+     * <p>
+     * 字符          数值
+     * I             1
+     * V             5
+     * X             10
+     * L             50
+     * C             100
+     * D             500
+     * M             1000
+     * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+     * <p>
+     * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，
+     * 所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+     * <p>
+     * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+     * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+     * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+     * 给定一个整数，将其转为罗马数字。输入确保在 1 到 3999 的范围内。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 3
+     * 输出: "III"
+     * 示例 2:
+     * <p>
+     * 输入: 4
+     * 输出: "IV"
+     * 示例 3:
+     * <p>
+     * 输入: 9
+     * 输出: "IX"
+     * 示例 4:
+     * <p>
+     * 输入: 58
+     * 输出: "LVIII"
+     * 解释: L = 50, V = 5, III = 3.
+     * 示例 5:
+     * <p>
+     * 输入: 1994
+     * 输出: "MCMXCIV"
+     * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/integer-to-roman
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * 字符          数值
+     * I             1
+     * V             5
+     * X             10
+     * L             50
+     * C             100
+     * D             500
+     * M             1000
+     */
+    public static String intToRoman(int num) {
+        StringBuilder builder = new StringBuilder();
+        return builder.toString();
+    }
+
+    public static int getResult(long num) {
+
+        int sum = 1;
+        while (num != 0) {
+            sum = sum * 10;
+            num--;
+        }
+        return sum;
     }
 }
--- src/com/dai/otheralgo/LeetCodeTest2.java
+++ src/com/dai/otheralgo/LeetCodeTest2.java
@@ -8,7 +8,12 @@ public class LeetCodeTest2 {
 //        String s = "LEETCODEISHIRING";
 //        convert(s, 3);
 
-        intToRoman(1994);
+//        intToRoman(1994);
+        String[] strings = {"abab", "aba", ""};
+        String result = longestCommonPrefix(strings);
+        System.out.println("result = " + result);
+        String ab = "adbad";
+        System.out.println("ab.substring(0,0) = " + ab.substring(4, 5));
     }
 
     public static String convert(String s, int numRows) {
@@ -30,7 +35,6 @@ public class LeetCodeTest2 {
 
         return array.toString();
     }
-
 
     /**
      * 整数转罗马数字
@@ -93,15 +97,50 @@ public class LeetCodeTest2 {
         map.put("M", 1000);
 
         int result = 0;
-        for(int i = 0;i < s.length();) {
-            if(i + 1 < s.length() && map.containsKey(s.substring(i, i+2))) {
-                result += map.get(s.substring(i, i+2));
+        for (int i = 0; i < s.length(); ) {
+            if (i + 1 < s.length() && map.containsKey(s.substring(i, i + 2))) {
+                result += map.get(s.substring(i, i + 2));
                 i += 2;
             } else {
-                result += map.get(s.substring(i, i+1));
-                i ++;
+                result += map.get(s.substring(i, i + 1));
+                i++;
             }
         }
         return result;
+    }
+
+    /**
+     * 编写一个函数来查找字符串数组中的最长公共前缀。
+     * <p>
+     * 如果不存在公共前缀，返回空字符串 ""。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: ["flower","flow","flight"]
+     * 输出: "fl"
+     * 示例 2:
+     * <p>
+     * 输入: ["dog","racecar","car"]
+     * 输出: ""
+     * 解释: 输入不存在公共前缀。
+     * 说明:
+     * <p>
+     * 所有输入只包含小写字母 a-z 。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/longest-common-prefix
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static String longestCommonPrefix(String[] strs) {
+
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+
     }
 }
